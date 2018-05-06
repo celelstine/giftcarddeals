2e2dc619becf4cb2cb5ccad5277853ef7b5d633f
celelstine <okorocelestine@gmail.com>
2018-05-06 08:56:26 +0100

    initial commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import hbs from 'express-hbs';
 import methodOverride from 'method-override';
 // package to log error on console
 import logger from 'morgan';
@@ -36,11 +37,23 @@ app.use(fileUpload());
 // Lets us use HTTP verbs such as PUT or DELETE in places where the client doesn't support it.
 // https://www.npmjs.com/package/method-override
 app.use(methodOverride());
+
 // serve static files in public folder
-
 const publicPath = path.join(__dirname, 'dist/');
-
 app.use(express.static(publicPath));
+// set the view engine
+app.set('view engine', 'hbs');
+
+// configure the view engine 
+app.engine('hbs', hbs.express4({  
+  defaultLayout: __dirname + '/views/layouts/default.hbs',
+  partialsDir: __dirname + '/views/partials',
+  layoutsDir: __dirname + '/views/layouts'
+}));
+
+// configure views path
+app.set('views', path.join(__dirname,'/views'));
+
 // server compressed javascript file
 app.get('*.js', (req, res, next) => {
   req.url = `${req.url}.gz`;
@@ -54,11 +67,52 @@ app.use('/api/v1', router);
 
 
 
-app.all('*', (req, res) => {
-  console.log('serving, ', `${publicPath}index.html`);
+app.all('/admin', (req, res) => {
   res.sendFile(`${publicPath}index.html`)
 });
 
+app.get('/termsAndConditions', (req, res) => {
+  res.render('termsandConditions');
+})
+app.get('/', function(req, res) {  
+  var user = {
+    first: 'Brian',
+    last: 'Mancini',
+    site: 'http://derpturkey.com',
+    age: 32
+  }
+  var products = [
+    {"id":1,"name":"USA Itunes card","acronym":"usi","rate":220,"highDenominationRate":100,"isActive":true,"image_url":"https://www.exchangezone9ja.com/productImages/itunes-gift-card-pile.4728efc.png","extra":"","cardCurrency":"$","createdAt":"2018-03-07T16:22:35.000Z","updatedAt":"2018-05-01T17:19:33.000Z"},{"id":5,"name":"Steam Card","acronym":"StC","rate":225,"highDenominationRate":225,"isActive":true,"image_url":"https://www.exchangezone9ja.com/productImages/Steam-wallet-cards-south-africa.jpg","extra":"","cardCurrency":"$","createdAt":"2018-03-22T21:52:56.000Z","updatedAt":"2018-04-25T14:27:55.000Z"},{"id":6,"name":"Amazon Cards","acronym":"AMC","rate":240,"highDenominationRate":240,"isActive":true,"image_url":"https://www.exchangezone9ja.com/productImages/amazon-gift-cards-fanned.png","extra":"","cardCurrency":"$","createdAt":"2018-03-22T22:13:13.000Z","updatedAt":"2018-04-27T06:16:47.000Z"},{"id":7,"name":"Australian Itunes Card","acronym":"AIC","rate":120,"highDenominationRate":120,"isActive":true,"image_url":"https://www.exchangezone9ja.com/productImages/itunes-gift-card-pile.4728efc.png","extra":"","cardCurrency":"$","createdAt":"2018-03-23T06:58:24.000Z","updatedAt":"2018-04-18T08:40:08.000Z"},{"id":8,"name":"Canadian Itune Cards","acronym":"CIC","rate":120,"highDenominationRate":120,"isActive":true,"image_url":"https://www.exchangezone9ja.com/productImages/itunes-gift-card-pile.4728efc.png","extra":"","cardCurrency":"$","createdAt":"2018-03-23T06:59:16.000Z","updatedAt":"2018-04-18T08:40:01.000Z"}
+  ];
+  var banks = [
+    'Access Bank',
+    'Citibank',
+    'Diamond Bank',
+    'Ecobank Nigeria',
+    'Enterprise Bank Limited',
+    'Fidelity Bank Nigeria',
+    'First Bank of Nigeria',
+    'First City Monument Bank',
+    'FSDH Merchant Bank',
+    'Guaranty Trust Bank',
+    'Heritage Bank Plc.',
+    'Keystone Bank Limited',
+    'Mainstreet Bank Limited',
+    'Rand Merchant Bank',
+    'Savannah Bank',
+    'Skye Bank',
+    'Stanbic IBTC Bank Nigeria Limited',
+    'Standard Chartered Bank',
+    'Sterling Bank',
+    'Union Bank of Nigeria',
+    'United Bank for Africa',
+    'Unity Bank Plc.',
+    'Wema Bank',
+    'Zenith Bank',
+  ];
+  res.render('index', { user, products, banks });
+
+});
 
 // catch errors
 app.use((err, req, res, next) => {
